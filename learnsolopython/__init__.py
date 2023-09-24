8dcd33f8a597a0cee7e6413d875fc6dcab16c6ec
Emotional-emptiness <moon.linexx@gmail.com>
2023-09-24 11:33:32 +0300

    first steps in python after java

---
+++ learnsolopython/__init__.py
@@ -0,0 +1,12 @@
+print("hello from java")
+a = 10
+b = 29
+print(a + b)
+lists = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lists)
+array = 1, 2, 3, 4
+print(array.__getitem__(0))
+liststring = ["a", "b", "c", "f", "e", "x", "z", "x", "c"]
+print(liststring + lists)
+print(sorted(liststring))
+print("get item 3 (4) -> " + liststring.__getitem__(3))
